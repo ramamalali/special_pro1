4a65457c78e2ebec3b4657b7ca8f484363c5e03b
ali darwish <alidarwish502@gmail.com>
2023-10-19 23:39:07 +0300

    Configured the routes for the project.

--- src/routing/routes.jsx
+++ src/routing/routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../modules/Home/Home";
+import Login from "../modules/utlis/login/Login";
 import Layout from "./Layout";
+import Aboutus from "../modules/AboutUs/pages/Aboutus";
 
 const router = createBrowserRouter([
   {
@@ -9,9 +11,17 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home/>
-      }
-    ]
+        element: <Home />,
+      },
+      {
+        path: "sign-in",
+        element: <Login />,
+      },
+      {
+        path: "about",
+        element: <Aboutus />,
+      },
+    ],
   },
 ]);
 
